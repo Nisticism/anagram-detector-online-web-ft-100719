8a212442bf5f6886292217b429eac3def93a0221
Hans Foster <fosterhans@gmail.com>
2019-11-06 06:21:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,14 +10,14 @@ class Anagram
   
   def match(words)
     word_letters = @word.split("")
+    word_letters.sort
     new_array = []
-    words.each do |word|
-      temp_array = word.split("")
+    words.each do |array_word|
+      temp_array = array_word.split("")
       unsorted_temp_array = temp_array
       sorted_temp_array = temp_array.sort
-      word_letters.sort
       if sorted_temp_array == word_letters
-        new_array.push(unsorted_temp_array.join(""))
+        new_array << unsorted_temp_array.join("")
       end
     end
     new_array
