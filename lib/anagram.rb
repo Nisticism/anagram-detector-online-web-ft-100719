541dd807e3c773a8ed1513a9e43d1a929152febd
Hans Foster <fosterhans@gmail.com>
2019-11-06 06:19:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,18 @@ class Anagram
   end
   
   def match(words)
-    words.each do || 
+    word_letters = @word.split("")
+    new_array = []
+    words.each do |word|
+      temp_array = word.split("")
+      unsorted_temp_array = temp_array
+      sorted_temp_array = temp_array.sort
+      word_letters.sort
+      if sorted_temp_array == word_letters
+        new_array.push(unsorted_temp_array.join(""))
+      end
+    end
+    new_array
   end
   
   
